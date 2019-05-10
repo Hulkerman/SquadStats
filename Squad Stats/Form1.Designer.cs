4e965dcbc5c7772fc889f359954ce2dc98464656
Joshua Koroluk <Joshua_Koroluk@sluz.ch>
2019-05-10 23:57:10 +0200

    initial commit

---
+++ Squad Stats/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Squad_Stats
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.requestStatsButton = new System.Windows.Forms.Button();
+            this.csgoStatsUrlTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // requestStatsButton
+            // 
+            this.requestStatsButton.Location = new System.Drawing.Point(12, 38);
+            this.requestStatsButton.Name = "requestStatsButton";
+            this.requestStatsButton.Size = new System.Drawing.Size(337, 20);
+            this.requestStatsButton.TabIndex = 1;
+            this.requestStatsButton.Text = "Get Stats";
+            this.requestStatsButton.UseVisualStyleBackColor = true;
+            this.requestStatsButton.Click += new System.EventHandler(this.RequestStatsButton_Click);
+            // 
+            // csgoStatsUrlTextBox
+            // 
+            this.csgoStatsUrlTextBox.Location = new System.Drawing.Point(12, 12);
+            this.csgoStatsUrlTextBox.Name = "csgoStatsUrlTextBox";
+            this.csgoStatsUrlTextBox.Size = new System.Drawing.Size(337, 20);
+            this.csgoStatsUrlTextBox.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 75);
+            this.Controls.Add(this.csgoStatsUrlTextBox);
+            this.Controls.Add(this.requestStatsButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button requestStatsButton;
+        private System.Windows.Forms.TextBox csgoStatsUrlTextBox;
+    }
+}
+
---
+++ Squad Stats/request.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace Squad_Stats
+{
+    public class Request
+    {
+        public Request()
+        {
+
+        }
+
+        public void getCsgoStatsHtml(string csgoStatsUrl)
+        {
+            if (csgoStatsUrl == "")
+                return;
+
+            // TODO: Implement local html read
+
+            //HtmlWeb web = new HtmlWeb();
+            //var htmlDoc = web.Load(csgoStatsUrl);
+            //var nodes = htmlDoc.DocumentNode.SelectNodes("//p");
+
+            //foreach (var node in nodes) {
+            //    Console.WriteLine("\nNode Name: " + node.Name + "\n" + node.OuterHtml);
+            //}
+        }
+    }
+}
